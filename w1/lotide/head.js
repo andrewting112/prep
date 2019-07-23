3495b99586a64eba1412704e078e95cf86ebc556
Andrew Ting <andrewting112@gmail.com>
2019-07-23 16:50:54 +0000

    week 1 work

--- w1/lotide/head.js
+++ w1/lotide/head.js
@@ -12,3 +12,17 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 // done
+
+const head = function(arr) {
+  return arr[0];
+
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
+
+
+
+
